112af4c1ed3aebad0859fcfd9694b3415d35e05b
Juan Veronez <juan.monteiro@venturus.org.br>
2022-10-30 17:43:26 -0300

    creates the first token page: colors
    - creates a component Colors Grid
    - adds polished as color helper lib

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,18 @@
+import { colors } from '@ignite-ui/tokens'
+import { getContrast } from 'polished'
+export const ColorsGrid = () =>
+  Object.entries(colors).map(([token, color]) => (
+    <div key={token} style={{ backgroundColor: color, padding: '2rem' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          fontFamily: 'monospace',
+          color: getContrast('#FFF', color) > 3.5 ? '#ddd' : '#222',
+        }}
+      >
+        <strong>${token}</strong>
+        <span>{color}</span>
+      </div>
+    </div>
+  ))
