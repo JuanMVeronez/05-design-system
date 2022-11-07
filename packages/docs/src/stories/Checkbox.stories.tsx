f6b6559c1829b397003bb9be7007cafd3d35ce43
Juan Veronez <juan.monteiro@venturus.org.br>
2022-11-07 18:22:14 -0300

    creates checkbox component and story
    - adds radix-ui checkbox as dependency to do it

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Box, Checkbox, CheckboxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {
+    placeholder: 'Type your username',
+  },
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+      >
+        {Story()}
+        <Text>Accept terms and conditions</Text>
+      </Box>
+    ),
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {
+  args: {},
+}
+
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true,
+  },
+}
